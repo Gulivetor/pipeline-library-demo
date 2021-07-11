281e7afe11237ba2f4d0b7c1b620d9ed2c8a3524
AJR <adamjr1083@yahoo.com>
2021-07-11 15:28:17 +0300

    modified webhook_test.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -4,5 +4,5 @@ def call(String repo_name = 'jenkinsfiles') {
     echo "Now cloning repo: ${repo_name}."
     echo 'Branch name : ' + BRANCH
     echo 'URL : ' + SSH_URL
-    git branch: BRANCH, credentialsId: 'jenkins-github', url: 'git@github.com:Gulivetor/jenkinsfiles.git'
+    git branch: BRANCH, credentialsId: 'jenkins-github', url: SSH_URL
 }
