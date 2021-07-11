651619fad3689054b18da2d203fbe2c7c25b4cf9
AJR <adamjr1083@yahoo.com>
2021-07-11 21:08:33 +0300

    modified cloneRepo

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -5,4 +5,5 @@ def call(String repo_name = 'jenkinsfiles') {
     echo 'Branch name : ' + BRANCH
     echo 'URL : ' + SSH_URL
     git branch: BRANCH, credentialsId: 'jenkins-github', url: SSH_URL
+    build job: "simple-node-js-react-npm-app"
 }
